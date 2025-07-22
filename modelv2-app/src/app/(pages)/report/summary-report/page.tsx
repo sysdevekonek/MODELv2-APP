e3cc9a05aba04905ff41ddc89edaf9a400aabfe3
yubanji <boris.garcia@ekonek.com>
2025-07-22 14:28:48 +0800

    #78485 Summary Report Module
    Added:
    - comboBox.tsx (reusable component for fields with dropdown selections)
    -data.ts (file for data types to all module)
    
    Installed:
    - exceljs
    - file-saver

--- modelv2-app/src/app/(pages)/report/summary-report/page.tsx
+++ modelv2-app/src/app/(pages)/report/summary-report/page.tsx
@@ -1,8 +1,20 @@
-export default function SummaryReport() {
+"use client";
+import SummaryReportFields from "@/components/features/reports/SummaryReportFields";
+
+export default function SummaryReportPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center text-gray-600">
-      <h1 className="text-3xl font-bold mb-2">🚧 Under Maintenance</h1>
-      <p className="text-lg">This feature is not yet available. Please check back soon.</p>
+    <div className="bg-bgDefcont w-full h-full rounded-[5px] shadow-lg relative">
+      <div className="bg-mainDef3 text-white font-semibold p-4 inline-block rounded-[5px] mb-4 -translate-y-1/4 -top-6">
+        <h1>SUMMARY REPORT</h1>
+      </div>
+
+      <div className="p-4">
+        <div className="bg-mainDef3 text-white font-semibold w-full p-3 pl-6 mb-1">
+          <label>Generate Report</label>
+        </div>
+
+        <SummaryReportFields />
+      </div>
     </div>
   );
 }
---
+++ modelv2-app/src/components/features/reports/SummaryReportFields.tsx
@@ -0,0 +1,96 @@
+import React, { useRef } from "react";
+import { useSummaryReports } from "@/hooks/useSummaryReport";
+import ComboBox, { ComboBoxRef } from "@/components/comboBox";
+
+export default function SummaryReportFields() {
+  const comboRef = useRef<ComboBoxRef>(null);
+  const {
+    clients,
+    selectedClient,
+    fromDate,
+    toDate,
+    loading,
+    setSelectedClient,
+    setFromDate,
+    setToDate,
+    fetchAndExportReport,
+    clearForm,
+  } = useSummaryReports();
+
+  const handleClear = () => {
+    clearForm();
+    comboRef.current?.clear(); 
+  };
+
+  return (
+    <>
+      <form className="flex items-center justify-center flex-col my-4">
+        <div className="flex items-center gap-x-4 mb-4">
+          <label className="text-medium text-mainTextDef1 w-32">
+            <span className="text-mainDef3 font-semibold">Client:</span>
+          </label>
+          <ComboBox
+            ref={comboRef}
+            items={clients}
+            displayKey="CMP_CON_NAM"
+            valueKey="CMP_CON_COD"
+            selectedValue={selectedClient}
+            setSelectedValue={setSelectedClient}
+            className="text-xs h-10 px-4 w-full border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-400 text-slate-700"
+
+          />
+        </div>
+
+        <div className="flex items-center gap-x-4 mb-2">
+          <label htmlFor="entryDateFrom" className="text-medium text-mainTextDef1 w-32">
+            <span className="text-mainDef3 font-semibold">Entry Date From:</span>
+          </label>
+          <input
+            id="entryDateFrom"
+            type="date"
+            value={fromDate}
+            onChange={(e) => setFromDate(e.target.value)}
+            className="w-80 text-xs h-10 px-4 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-400 text-slate-700"
+          />
+        </div>
+
+        <div className="flex items-center gap-x-4 mb-4">
+          <label htmlFor="entryDateTo" className="text-medium text-mainTextDef1 w-32">
+            <span className="text-mainDef3 font-semibold">Entry Date To:</span>
+          </label>
+          <input
+            id="entryDateTo"
+            type="date"
+            value={toDate}
+            onChange={(e) => setToDate(e.target.value)}
+            className="w-80 text-xs h-10 px-4 border border-slate-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-400 text-slate-700"
+          />
+        </div>
+      </form>
+
+      <div className="flex items-center justify-center gap-4">
+        <button
+          onClick={fetchAndExportReport}
+          disabled={loading}
+          className={loading ? "pointer-events-none opacity-50" : ""}
+        >
+          <div className="h-10 w-40 bg-mainDef3 rounded flex justify-center items-center hover:bg-mainDef2 hover:text-mainTextDef2 transition">
+        <span className="text-white font-semibold">
+          {loading ? "LOADING..." : "GENERATE"}
+        </span>
+          </div>
+        </button>
+
+        <button
+          onClick={handleClear}
+          disabled={loading}
+          className={loading ? "pointer-events-none opacity-50" : ""}
+        >
+          <div className="h-10 w-40 bg-mainDef2 rounded flex justify-center items-center hover:bg-mainDef2 text-white font-semibold">
+        CLEAR
+          </div>
+        </button>
+      </div>
+    </>
+  );
+}
